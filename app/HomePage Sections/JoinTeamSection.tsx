8c3616c9aba7fb729788eff1bd807b154816dd24
Fabian Ajokubi <favourajokubi@gmail.com>
2024-05-09 07:24:39 +0100

    Adjust join us for mobile

--- app/HomePage Sections/JoinTeamSection.tsx
+++ app/HomePage Sections/JoinTeamSection.tsx
@@ -4,22 +4,23 @@ import Image from "next/image";
 
 export default function JoinTeamSection() {
     return (
-        <section className="sm:pt-32 pt-24 sm:px-[12.5vw] px-3 bg-white/80 min-h-screen">
-            <div>
-                <h1 className={clsx("sm:text-[5vw] font-black text-[#6fad45] text-5xl text-center mx-auto md:max-w-[60vw] text-balance")}>
+        <section className="sm:pt-32 pt-24 sm:px-[12.5vw] px-3 bg-white/80 min-h-screen flex flex-col">
+            <div className="flex-1">
+                <h1 className={clsx("sm:text-[5vw] sm:leading-[5.25vw] font-black text-[#6fad45] text-4xl text-center mx-auto sm:max-w-[70vw] max-w-[85vw] text-balance")}>
                     <span className="bg-[#6fad45]/80 text-white">Join Our Team</span> and Be Part of Something Special!
                 </h1>
 
-                <p className="text-center p-5 text-lg md:max-w-[60vw] mx-auto">
-                    At AmeriCare, we don&apos;t just offer jobs; we provide opportunities to make a real difference in people’s lives. If you’re seeking a fulfilling career in a dynamic, diverse, and people-first home care company, you’ve come to the right place. Our commitment to compassionate care, ongoing training, and career advancement creates an exceptional work environment where you can thrive both personally and professionally.
+                <p className="text-center p-5 sm:text-lg opacity-70 md:max-w-[60vw] mt-3 mx-auto flex flex-col gap-3">
+                    <span>At <span className="bg-[#6fad45]/80 text-white">AmeriCare</span>, we don&apos;t just offer jobs; we provide opportunities to make a real difference in people&apos;s lives. If you&apos;re seeking a fulfilling career in a dynamic, diverse, and people-first home care company, you&apos;ve come to the right place. </span>
+                    <span>Our commitment to compassionate care, ongoing training, and career advancement creates an exceptional work environment where you can thrive both personally and professionally.</span>
                 </p>
-            </div>
-            <div className="w-fit mx-auto pt-5">
-                <CustomBtn
-                    customClass="text-base"
-                    linkHref={""}
-                    text={"Join us"}
-                />
+                <div className="w-fit mt-3 mx-auto pt-5">
+                    <CustomBtn
+                        customClass="text-base"
+                        linkHref={""}
+                        text={"Join us"}
+                    />
+                </div>
             </div>
 
             <div className="mt-12">
@@ -30,7 +31,7 @@ export default function JoinTeamSection() {
                     width={938}
                     priority
                     className={clsx(
-                        "h-[30rem] w-auto smooth mx-auto"
+                        "w-[clamp(40rem,50vw,60rem)] object-contain smooth mx-auto"
                     )}
                 />
             </div>
