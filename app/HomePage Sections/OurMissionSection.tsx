0bd036f88f54f6fdfcdd914b6c67103a2bf760a5
Vinwilkinson <vin.wilkinson11@gmail.com>
2024-05-13 16:50:38 +0100

    retouch Our mission section

--- app/HomePage Sections/OurMissionSection.tsx
+++ app/HomePage Sections/OurMissionSection.tsx
@@ -9,7 +9,7 @@ export default function OurMissionSection() {
         <>
             <div className="py-12 sm:px-[12.5vw] px-3 relative z-10 bg-white">
                 <div className="mb-12">
-                    <h1 className={clsx(FontFamily.className, "sm:text-[4vw] text-5xl text-primary")}>Our Mission</h1>
+                    <h1 className={clsx(FontFamily.className, "sm:text-[4vw] text-5xl text-primary font-semibold")}>Our Mission</h1>
                     <p className="opacity-60 text-[0.97rem] font-semibold">Compassionate Care, Right at Home</p>
                 </div>
 
