1cef13be17765131d663df6ac63b116ad5918994
Vinwilkinson <vin.wilkinson11@gmail.com>
2024-05-24 16:27:17 +0100

    Update WhatWeOffer

--- lib/WhatWeOffer.ts
+++ lib/WhatWeOffer.ts
@@ -1,11 +1,11 @@
 export const WhatWeOffer: { title: string; content: string }[] = [
     {
         title: "Companion Care",
-        content: "Everyone needs companionship. If that’s someone to help with household chores, getting from one place to another, or even just putting a puzzle together, Right at Home’s caregivers help keep your loved one engaged and active in life."
+        content: "Everyone needs companionship. Whether that's someone to help with household chores, getting from one place to another, or even just putting a puzzle together. AmeriCare’s caregivers help your loved ones remain active and engaged in life."
     },
     {
         title: "Personal Care",
-        content: "A daily routine can be one of the strongest reminders of what makes home feel like home. When those day-to-day tasks get harder, they can also be one of the first things to slip. Right at Home’s caregivers are there to help keep everything on track."
+        content: "A daily routine can be one of the strongest reminders of what makes home feel like home. When those day-to-day tasks get harder, they can also be one of the first things to slip. AmeriCare’s caregivers are there to help keep everything on track."
     },
     {
         title: "Nursing Care",
@@ -13,6 +13,6 @@ export const WhatWeOffer: { title: string; content: string }[] = [
     },
     {
         title: "Specialty Care",
-        content: "Our caregivers are trained to help with a number of specialized services for chronic, and often demanding, medical conditions like Alzheimer’s, diabetes, and Parkinson’s. We also offer hospice support."
+        content: "Our caregivers are trained to help with a number of specialized services for chronic, and often demanding, medical conditions like Alzheimer’s, Diabetes, and Parkinson’s. We also offer hospice support."
     }
 ];
