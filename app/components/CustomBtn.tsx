c343c695b36cc0b4b6d3c04288689ea8fee47a70
Vinwilkinson <vin.wilkinson11@gmail.com>
2024-06-07 11:01:26 +0100

    Update CustomBtn

--- app/components/CustomBtn.tsx
+++ app/components/CustomBtn.tsx
@@ -1,7 +1,7 @@
 import clsx from "clsx";
 import Link from "next/link";
 
-export default function CustomBtn(btnProps: { customClass?: string, text: string, linkHref: string, noBorder?: boolean, external?: boolean }) {
+export default function CustomBtn(btnProps: { customClass?: string, text: string, linkHref: string, noBorder?: boolean, external?: boolean, inView?: boolean }) {
     return (
         <Link 
             href={btnProps.linkHref} 
@@ -9,8 +9,9 @@ export default function CustomBtn(btnProps: { customClass?: string, text: string
             className={clsx(
             btnProps.customClass ? btnProps.customClass : "",
             btnProps.noBorder? "border-transparent": "border-white dark:border-darkBg",
-            "font-semibold smooth dark:bg-primary/25 dark:text-white bg-primary/5 sm:py-4 py-2 sm:px-12 whitespace-nowrap px-6 active:rotate-3 hover:text-white cursor-pointer active:scale-90 text-primary outline-primary w-fit select-none border-4 outline-2 outline-dashed relative overflow-hidden h-fit",
+            "font-semibold smooth dark:bg-primary/25 dark:text-white bg-primary/5 sm:py-3 py-2 sm:px-10 whitespace-nowrap px-6 active:rotate-3 hover:text-white cursor-pointer active:scale-90 text-primary outline-primary w-fit select-none border-4 outline-2 outline-dashed relative overflow-hidden h-fit",
             "after:h-2 after:w-2 sm:rounded-xl rounded-lg hover:after:bg-primary hover:after:h-[105%] hover:after:w-full smooth-after after:absolute after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2",
+            btnProps.inView ? "outline-white text-white": "",
         )}>
             <span className="relative z-10">{btnProps.text}</span>
         </Link>
--- app/components/NavSection.tsx
+++ app/components/NavSection.tsx
@@ -84,6 +84,7 @@ export default function NavigationComponent({ variation, inViewContainer }: { va
                         text="Contact us"
                         linkHref="#"
                         noBorder={inViewContainer}
+                        inView={inViewContainer}
                         customClass={clsx(
                             inViewContainer ? "border-transparent outline-black text-black" : ""
                         )}
