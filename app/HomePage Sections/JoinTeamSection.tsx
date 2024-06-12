983e7a7a6057c9496bf915ca46bffeb205a6c59c
Fabian Ajokubi <favourajokubi@gmail.com>
2024-06-12 12:55:44 +0100

    Fix Join Us btn

--- app/HomePage Sections/JoinTeamSection.tsx
+++ app/HomePage Sections/JoinTeamSection.tsx
@@ -24,14 +24,12 @@ export default function JoinTeamSection() {
                 <p className="text-center p-5 sm:text-lg opacity-70 md:max-w-[60vw] mt-3 mx-auto flex flex-col gap-3">
                     <span>At AmeriCare, we don&apos;t just offer jobs; we provide opportunities to make a real difference in people&apos;s lives. If you&apos;re seeking a fulfilling career in a dynamic, diverse, and people-first home care company, you&apos;ve come to the right place. </span>
                     <span>Our commitment to compassionate care, ongoing training, and career advancement creates an exceptional work environment where you can thrive both personally and professionally.</span>
-                </p>
-                <div className="w-full mt-3 mx-auto pt-5">
                     <CustomBtn
-                        customClass="text-base border-4 overflow-hidden"
+                        customClass="text-base border-4 overflow-hidden mx-auto mt-8"
                         linkHref={"/careers"}
                         text={"Join us"}
                     />
-                </div>
+                </p>
             </div>
 
             <div className="mt-24">
