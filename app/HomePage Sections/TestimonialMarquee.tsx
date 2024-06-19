46fc90a7826da0dcafe1c3f5c4fb90715103e0cc
Fabian Ajokubi <favourajokubi@gmail.com>
2024-06-19 03:08:50 +0100

    Update Fix 01

--- app/HomePage Sections/TestimonialMarquee.tsx
+++ app/HomePage Sections/TestimonialMarquee.tsx
@@ -7,12 +7,13 @@ const FontFamily = Playfair_Display({ subsets: ["latin"], weight: "600" });
 
 export default function TestimonialMarquee() {
     return (
-        <div className="sm:py-8 py-4 relative z-10 dark:bg-darkBg dark:text-white bg-white">
-            <div className="py-24 2xl:px-[10vw] sm:px-[8vw]  px-6 mb-12">
-                <h1 className={clsx(FontFamily.className, "sm:text-[4vw] text-5xl text-primary font-semibold")}>Hear From Our Community</h1>
+        <div className="sm:py-8 py-4 relative z-10 dark:bg-darkBg dark:text-white bg-white overflow-hidden">
+            <div className="pt-24 pb-12 2xl:px-[10vw] sm:px-[8vw] px-6 mb-6">
+                <h1 className={clsx(FontFamily.className, "sm:text-[3vw] text-4xl text-primary font-semibold relative z-10")}>Hear From Our Community</h1>
+                <h1 className={clsx(FontFamily.className, "absolute top-1/2 -translate-y-1/2 left-1/2 whitespace-nowrap -translate-x-1/2 sm:text-[15vw] text-[25vw] dark:text-[rgba(255,255,255,0.015)] text-[rgba(111,173,69,0.015)] pointer-events-none select-none font-bold")}>Our Community</h1>
             </div>
             <Marquee
-                className="py-4"
+                className="py-4 z-10"
                 pauseOnHover
                 speed={30}
             >
