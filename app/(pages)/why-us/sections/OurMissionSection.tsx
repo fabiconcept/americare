9e8b5afbbe3ad2f1ab05859646e31202ff759846
Fabian Ajokubi <favourajokubi@gmail.com>
2024-06-22 17:30:08 +0100

    Fully Animate the entrance of the Why Page sections

--- app/(pages)/why-us/sections/OurMissionSection.tsx
+++ app/(pages)/why-us/sections/OurMissionSection.tsx
@@ -1,48 +1,60 @@
+import InViewWrapper from "@/app/components/InViewWrapper";
 import clsx from "clsx";
+import { Variants } from "framer-motion";
 import { Playfair_Display } from "next/font/google";
 import Image from "next/image";
 const FontFamily = Playfair_Display({ subsets: ["latin"], weight: "600" });
 
 
 export default function OurMissionSection() {
+    const cardIn: Variants = {
+        hidden: { opacity: 0, translateX: 100, },
+        visible: { opacity: 1, translateX: 0, transition: { duration: 0.5 } },
+    };
+    const cardInAlt: Variants = {
+        hidden: { opacity: 0, translateX: -100, },
+        visible: { opacity: 1, translateX: 0, transition: { duration: 0.5 } },
+    };
     return (
         <>
             <div className="dark:bg-darkBg dark:text-white grid place-items-center sm:py-24 py-12 2xl:px-[10vw] sm:px-[8vw]  px-6 relative z-10 bg-white">
                 <div>
-                    <div className="mb-12">
+                    <InViewWrapper animation={cardIn} className="mb-12">
                         <h1 className={clsx(FontFamily.className, "sm:text-[4vw] text-5xl text-primary font-semibold")}>Our Mission</h1>
-                    </div>
+                    </InViewWrapper>
 
                     <div className="flex gap-12 flex-wrap">
-                        <p className="min-w-[20rem] flex-[2] h-fit 2xl:text-5xl sm:text-4xl text-2xl sm:font-normal font-semibold opacity-80 relative">
-                            <Image
-                                src={"https://americare.sirv.com/icons/quote-icon.svg"}
-                                alt="Quote icon"
-                                height={300}
-                                width={300}
-                                priority
-                                className={clsx(
-                                    "w-[8vw] opacity-40 smooth absolute -top-[2.5vw] -left-[6vw] -rotate-3 -z-[1]"
-                                )}
-                            />
-                            <span className="sm:text-left text-center">
-                                Our mission is to ensure the well-being and happiness of every member of your family, from the youngest to the oldest.
-                            </span>
-                            <Image
-                                src={"https://americare.sirv.com/icons/quote-icon.svg"}
-                                alt="Quote icon"
-                                height={300}
-                                width={300}
-                                priority
-                                className={clsx(
-                                    "w-[8vw] opacity-40 smooth absolute -bottom-[2.5vw] rotate-3 mix-blend-lighten scale-x-[-1] -right-[2vw] -z-[1]"
-                                )}
-                            />
-                        </p>
+                        <InViewWrapper animation={cardIn} className="min-w-[20rem] flex-[2] h-fit 2xl:text-5xl sm:text-4xl text-2xl sm:font-normal font-semibold opacity-80 relative">
+                            <>
+                                <Image
+                                    src={"https://americare.sirv.com/icons/quote-icon.svg"}
+                                    alt="Quote icon"
+                                    height={300}
+                                    width={300}
+                                    priority
+                                    className={clsx(
+                                        "w-[8vw] opacity-40 smooth absolute -top-[2.5vw] -left-[6vw] -rotate-3 -z-[1]"
+                                    )}
+                                />
+                                <span className="sm:text-left text-center">
+                                    Our mission is to ensure the well-being and happiness of every member of your family, from the youngest to the oldest.
+                                </span>
+                                <Image
+                                    src={"https://americare.sirv.com/icons/quote-icon.svg"}
+                                    alt="Quote icon"
+                                    height={300}
+                                    width={300}
+                                    priority
+                                    className={clsx(
+                                        "w-[8vw] opacity-40 smooth absolute -bottom-[2.5vw] rotate-3 mix-blend-lighten scale-x-[-1] -right-[2vw] -z-[1]"
+                                    )}
+                                />
+                            </>
+                        </InViewWrapper>
 
-                        <p className="min-w-[20rem] flex-1 opacity-70">
-                            We understand the unique needs and challenges that children and seniors face, and we&apos;re here to provide the highest level of care and support. Our approach is holistic encompassing mental and physical health to support the entire family through our a leading to the best outcomes.
-                        </p>
+                        <InViewWrapper animation={cardInAlt} className="min-w-[20rem] flex-1 opacity-70">
+                            <>We understand the unique needs and challenges that children and seniors face, and we&apos;re here to provide the highest level of care and support. Our approach is holistic encompassing mental and physical health to support the entire family through our a leading to the best outcomes.</>
+                        </InViewWrapper>
                     </div>
                 </div>
             </div>
--- app/(pages)/why-us/sections/OurVisionSection.tsx
+++ app/(pages)/why-us/sections/OurVisionSection.tsx
@@ -1,49 +1,61 @@
+import InViewWrapper from "@/app/components/InViewWrapper";
 import clsx from "clsx";
+import { Variants } from "framer-motion";
 import { Playfair_Display } from "next/font/google";
 import Image from "next/image";
 const FontFamily = Playfair_Display({ subsets: ["latin"], weight: "600" });
 
 
 export default function OurVisionSection() {
+    const cardIn: Variants = {
+        hidden: { opacity: 0, translateX: 100, },
+        visible: { opacity: 1, translateX: 0, transition: { duration: 0.5 } },
+    };
+    const cardInAlt: Variants = {
+        hidden: { opacity: 0, translateX: -100, },
+        visible: { opacity: 1, translateX: 0, transition: { duration: 0.5 } },
+    };
+
     return (
         <>
             <div className="dark:bg-darkBg dark:text-white grid place-items-center sm:py-24 py-12 2xl:px-[10vw] sm:px-[8vw]  px-6 relative z-10 bg-white">
                 <div>
-                    <div className="mb-12">
+                    <InViewWrapper animation={cardIn}  className="mb-12">
                         <h1 className={clsx(FontFamily.className, "sm:text-[4vw] text-5xl text-primary font-semibold")}>Our Vision</h1>
-                        <p className="opacity-60 text-[0.97rem] font-semibold">You&apos;ll never walk alone</p>
-                    </div>
- 
-                    <div className="flex gap-12 flex-wrap">
-                        <p className="min-w-[20rem] flex-[2] h-fit 2xl:text-5xl sm:text-4xl text-2xl sm:font-normal font-semibold opacity-80 relative">
-                            <Image
-                                src={"https://americare.sirv.com/icons/quote-icon.svg"}
-                                alt="Quote icon"
-                                height={300}
-                                width={300}
-                                priority
-                                className={clsx(
-                                    "w-[8vw] opacity-40 smooth absolute -top-[2.5vw] -left-[6vw] -rotate-3 -z-[1]"
-                                )}
-                            />
-                            <span className="sm:text-left text-center">
-                                To lead in compassionate, reliable home healthcare, fostering healthier, happier, and independent living.
-                            </span>
-                            <Image
-                                src={"https://americare.sirv.com/icons/quote-icon.svg"}
-                                alt="Quote icon"
-                                height={300}
-                                width={300}
-                                priority
-                                className={clsx(
-                                    "w-[8vw] opacity-40 smooth absolute -bottom-[2.5vw] rotate-3 mix-blend-lighten scale-x-[-1] -right-[2vw] -z-[1]"
-                                )}
-                            />
-                        </p>
+                    </InViewWrapper>
 
-                        <p className="min-w-[20rem] flex-1 opacity-70">
-                            At AmeriCare Inc, our vision is to redefine the standards of home healthcare. We aspire to be the leading provider of compassionate, reliable, and person-centered care services, fostering a healthier, happier, and more connected community. Our goal is to empower individuals to live their lives to the fullest, with dignity and independence, within the comfort of their homes.
-                        </p>
+                    <div className="flex gap-12 flex-wrap">
+                        <InViewWrapper animation={cardIn} className="min-w-[20rem] flex-[2] h-fit 2xl:text-5xl sm:text-4xl text-2xl sm:font-normal font-semibold opacity-80 relative">
+                            <>
+                                <Image
+                                    src={"https://americare.sirv.com/icons/quote-icon.svg"}
+                                    alt="Quote icon"
+                                    height={300}
+                                    width={300}
+                                    priority
+                                    className={clsx(
+                                        "w-[8vw] opacity-40 smooth absolute -top-[2.5vw] -left-[6vw] -rotate-3 -z-[1]"
+                                    )}
+                                />
+                                <span className="sm:text-left text-center">
+                                    To lead in compassionate, reliable home healthcare, fostering healthier, happier, and independent living.
+                                </span>
+                                <Image
+                                    src={"https://americare.sirv.com/icons/quote-icon.svg"}
+                                    alt="Quote icon"
+                                    height={300}
+                                    width={300}
+                                    priority
+                                    className={clsx(
+                                        "w-[8vw] opacity-40 smooth absolute -bottom-[2.5vw] rotate-3 mix-blend-lighten scale-x-[-1] -right-[2vw] -z-[1]"
+                                    )}
+                                />
+                            </>
+                        </InViewWrapper>
+
+                        <InViewWrapper animation={cardInAlt} className="min-w-[20rem] flex-1 opacity-70">
+                            <>At AmeriCare Inc, our vision is to redefine the standards of home healthcare. We aspire to be the leading provider of compassionate, reliable, and person-centered care services, fostering a healthier, happier, and more connected community. Our goal is to empower individuals to live their lives to the fullest, with dignity and independence, within the comfort of their homes.</>
+                        </InViewWrapper>
                     </div>
                 </div>
             </div>
