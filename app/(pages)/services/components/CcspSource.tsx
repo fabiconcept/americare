ccb466d3cca3ea3ef6fdfce50d75636d1b5b0912
Fabian Ajokubi <favourajokubi@gmail.com>
2024-06-22 16:34:24 +0100

    Add inView Animation to CcspSource

--- app/(pages)/services/components/CcspSource.tsx
+++ app/(pages)/services/components/CcspSource.tsx
@@ -1,53 +1,78 @@
 import CustomBtn from "@/app/components/CustomBtn";
+import InViewWrapper from "@/app/components/InViewWrapper";
 import clsx from "clsx";
+import { Variants } from "framer-motion";
 import { Playfair_Display } from "next/font/google";
 import Image from "next/image";
 const FontFamily = Playfair_Display({ subsets: ["latin"], weight: "600" });
 
 
 export default function CcspSource() {
+    const fadeIn: Variants = {
+        hidden: { opacity: 0, translateY: 50 },
+        visible: { opacity: 1, translateY: 0, transition: { duration: 0.5 } },
+    };
+
+        
+    const cardIn: Variants = {
+        hidden: { opacity: 0, translateX: 100, },
+        visible: { opacity: 1, translateX: 0, transition: { duration: 0.5 }},
+    };
+    const cardInAlt: Variants = {
+        hidden: { opacity: 0, translateX: 100, },
+        visible: { opacity: 1, translateX: 0, transition: { duration: 0.5 }},
+    };
+
     return (
         <>
-            <div className="grid place-items-center sm:py-6 py-4 relative z-10">
+            <InViewWrapper animation={fadeIn} className="grid place-items-center sm:py-6 py-4 px-1 relative z-10 overflow-x-hidden">
                 <div>
                     <div className="mb-4 mt-5">
                         <h1 className={clsx(FontFamily.className, "sm:text-[3vw] text-4xl text-primary font-semibold")}>What is CCSP/SOURCE?</h1>
                     </div>
 
                     <div className="flex items-stretch gap-8 flex-wrap dark:mix-blend-screen mix-blend-multiply mt-12">
-                        <p className="sm:min-w-[30rem] min-w-[20rem] flex-1 flex flex-col gap-6">
-                            <span className=" h-fit 2xl:text-5xl sm:text-3xl text-xl sm:font-normal font-semibold opacity-80 relative">
-                                <span className="sm:text-left text-center">
-                                    &quot;CCSP and SOURCE are Georgia Medicaid programs offering in-home support to help elderly and disabled individuals avoid long-term care facilities.&quot;
-                                </span>
-                            </span>
-                            <span className="opacity-70">
-                                The Community Care Services Program (CCSP) and Service Options Using Resources in a
-                                Community Environment (SOURCE) are Medicaid waiver programs designed to assist elderly
-                                and disabled individuals in Georgia. These programs offer in-home support services to help
-                                avoid long-term care facilities.
-                            </span>
-                            <CustomBtn
-                                linkHref="#check_eligibility"
-                                text="Check Eligibility"
+                        <InViewWrapper animation={cardIn} className="flex-1 sm:min-w-[30rem] min-w-[20rem]">
+                            <>
+                                <p className="flex flex-col gap-6">
+                                    <span className=" h-fit 2xl:text-5xl sm:text-3xl text-xl sm:font-normal font-semibold opacity-80 relative">
+                                        <span className="sm:text-left text-center">
+                                            &quot;CCSP and SOURCE are Georgia Medicaid programs offering in-home support to help elderly and disabled individuals avoid long-term care facilities.&quot;
+                                        </span>
+                                    </span>
+                                    <span className="opacity-70">
+                                        The Community Care Services Program (CCSP) and Service Options Using Resources in a
+                                        Community Environment (SOURCE) are Medicaid waiver programs designed to assist elderly
+                                        and disabled individuals in Georgia. These programs offer in-home support services to help
+                                        avoid long-term care facilities.
+                                    </span>
+                                    <CustomBtn
+                                        linkHref="#check_eligibility"
+                                        text="Check Eligibility"
 
-                            />
-                        </p>
-                        <div className="min-w-[20rem] rounded-lg flex-1 overflow-hidden">
-                            <Image
-                                src={"https://americare.sirv.com/pexels-rollz-19971193.jpg"}
-                                alt={"Couple in a park"}
-                                height={3735}
-                                width={5602}
-                                priority
-                                className={clsx(
-                                    "min-w-full h-full object-cover smooth",
-                                )}
-                            />
-                        </div>
+                                    />
+                                </p>
+                            </>
+                        </InViewWrapper>
+                        <InViewWrapper animation={cardInAlt} className="flex-1 min-w-[20rem]">
+                            <>
+                                <div className="rounded-lg overflow-hidden">
+                                    <Image
+                                        src={"https://americare.sirv.com/pexels-rollz-19971193.jpg"}
+                                        alt={"Couple in a park"}
+                                        height={3735}
+                                        width={5602}
+                                        priority
+                                        className={clsx(
+                                            "min-w-full h-full object-cover smooth",
+                                        )}
+                                    />
+                                </div>
+                            </>
+                        </InViewWrapper>
                     </div>
                 </div>
-            </div>
+            </InViewWrapper>
         </>
     );
 }
--- app/(pages)/services/components/MedicarePrograms.tsx
+++ app/(pages)/services/components/MedicarePrograms.tsx
@@ -6,37 +6,48 @@ import Gapp from "./Gapp";
 import CcspKeyBenefits from "./CcspKeyBenefits";
 import GappKeyBenefits from "./GappKeyBenefits";
 import Link from "next/link";
+import { Variants } from "framer-motion";
+import InViewWrapper from "@/app/components/InViewWrapper";
 const FontFamily = Playfair_Display({ subsets: ["latin"], weight: "600" });
 
 
 export default function MedicarePrograms() {
+    const fadeIn: Variants = {
+        hidden: { opacity: 0, translateY: 50 },
+        visible: { opacity: 1, translateY: 0, transition: { duration: 0.5 } },
+    };
+
     return (
         <>
             <div className="dark:bg-darkBg dark:text-white grid place-items-center sm:py-24 py-12 2xl:px-[10vw] sm:px-[8vw]  px-6 relative z-10 bg-white">
-                <Image
-                    src={"https://americare.sirv.com/icons/bbblurry.svg"}
-                    alt={"blur shape"}
-                    height={800}
-                    width={800}
-                    className="sm:w-[70rem] w-[80rem] absolute sm:-top-[20rem] -top-[8rem] sm:-left-[20rem] -left-[12rem] dark:opacity-50 max-md:dark:opacity-25"
-                />
-                <div>
-                    <div className="mb-12">
-                        <h1 className={clsx(FontFamily.className, "sm:text-[4vw] text-5xl text-primary font-semibold")}>Medicaid Programs</h1>
-                        <p className="opacity-60 text-[0.97rem] font-semibold">CCSP/SOURCE and GAPP | <Link href={"/"} className="active:rotate-6 origin-left">AmeriCare</Link> Home Health</p>
-                    </div>
+                <InViewWrapper animation={fadeIn}>
+                    <>
+                        <Image
+                            src={"https://americare.sirv.com/icons/bbblurry.svg"}
+                            alt={"blur shape"}
+                            height={800}
+                            width={800}
+                            className="sm:w-[70rem] w-[80rem] absolute sm:-top-[20rem] -top-[8rem] sm:-left-[20rem] -left-[12rem] dark:opacity-50 max-md:dark:opacity-25"
+                        />
+                        <div>
+                            <div className="mb-12">
+                                <h1 className={clsx(FontFamily.className, "sm:text-[4vw] text-5xl text-primary font-semibold")}>Medicaid Programs</h1>
+                                <p className="opacity-60 text-[0.97rem] font-semibold">CCSP/SOURCE and GAPP | <Link href={"/"} className="active:rotate-6 origin-left">AmeriCare</Link> Home Health</p>
+                            </div>
 
-                    <div className="">
-                        <div className="flex gap-12 flex-wrap">
-                            <p className="flex-1 opacity-70">
-                                At AmeriCare Home Health, we specialize in providing high-quality home health care services
-                                through Medicaid programs like CCSP, SOURCE, and GAPP.
-                                Our goal is to help you or your
-                                loved ones remain at home while receiving the care and support needed.
-                            </p>
+                            <div className="">
+                                <div className="flex gap-12 flex-wrap">
+                                    <p className="flex-1 opacity-70">
+                                        At AmeriCare Home Health, we specialize in providing high-quality home health care services
+                                        through Medicaid programs like CCSP, SOURCE, and GAPP.
+                                        Our goal is to help you or your
+                                        loved ones remain at home while receiving the care and support needed.
+                                    </p>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
+                    </>
+                </InViewWrapper>
 
                 <CcspSource />
                 <CcspKeyBenefits />
