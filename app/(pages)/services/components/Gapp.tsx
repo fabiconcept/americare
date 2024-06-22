034ba8fa5af8376d55210b036808de6cd51cf887
Fabian Ajokubi <favourajokubi@gmail.com>
2024-06-22 16:59:21 +0100

    Add inView Animation to Gapp & GappKeyBenefits

--- app/(pages)/services/components/Gapp.tsx
+++ app/(pages)/services/components/Gapp.tsx
@@ -1,14 +1,30 @@
 import CustomBtn from "@/app/components/CustomBtn";
+import InViewWrapper from "@/app/components/InViewWrapper";
 import clsx from "clsx";
+import { Variants } from "framer-motion";
 import { Playfair_Display } from "next/font/google";
 import Image from "next/image";
 const FontFamily = Playfair_Display({ subsets: ["latin"], weight: "600" });
 
 
 export default function Gapp() {
+    const fadeIn: Variants = {
+        hidden: { opacity: 0, translateY: 50 },
+        visible: { opacity: 1, translateY: 0, transition: { duration: 0.5 } },
+    };
+
+        
+    const cardIn: Variants = {
+        hidden: { opacity: 0, translateX: 100, },
+        visible: { opacity: 1, translateX: 0, transition: { duration: 0.5 }},
+    };
+    const cardInAlt: Variants = {
+        hidden: { opacity: 0, translateX: 100, },
+        visible: { opacity: 1, translateX: 0, transition: { duration: 0.5 }},
+    };
     return (
         <>
-            <div className="grid place-items-center sm:py-6 py-4 relative z-10">
+            <InViewWrapper animation={fadeIn}  className="grid place-items-center sm:py-6 py-4 relative z-10">
                 <div>
                     <div className="mb-4 mt-10">
                         <h1 className={clsx(FontFamily.className, "sm:text-[3vw] text-4xl text-primary font-semibold")}>
@@ -17,39 +33,47 @@ export default function Gapp() {
                     </div>
 
                     <div className="flex gap-8 flex-wrap dark:mix-blend-screen mix-blend-multiply mt-12">
-                        <p className="sm:min-w-[30rem] min-w-[20rem] flex-1 flex flex-col gap-6">
-                            <span className=" h-fit 2xl:text-5xl sm:text-3xl text-xl sm:font-normal font-semibold opacity-80 relative">
-                                <span className="sm:text-left text-center">
-                                    &quot;The Georgia Pediatric Program (GAPP) offers in-home nursing services for medically fragile children under 21, ensuring continuous, high-quality care.&quot;
-                                </span>
-                            </span>
-                            <span className="opacity-70">
-                                The Georgia Pediatric Program (GAPP) provides in-home nursing services for medically fragile
-                                children under 21. This program ensures that children with complex medical needs receive
-                                continuous, high-quality care in their homes.
-                            </span>
+                        <InViewWrapper animation={cardIn} className="sm:min-w-[30rem] min-w-[20rem] flex-1">
+                            <>
+                                <p className="flex flex-col gap-6">
+                                    <span className=" h-fit 2xl:text-5xl sm:text-3xl text-xl sm:font-normal font-semibold opacity-80 relative">
+                                        <span className="sm:text-left text-center">
+                                            &quot;The Georgia Pediatric Program (GAPP) offers in-home nursing services for medically fragile children under 21, ensuring continuous, high-quality care.&quot;
+                                        </span>
+                                    </span>
+                                    <span className="opacity-70">
+                                        The Georgia Pediatric Program (GAPP) provides in-home nursing services for medically fragile
+                                        children under 21. This program ensures that children with complex medical needs receive
+                                        continuous, high-quality care in their homes.
+                                    </span>
 
-                            <CustomBtn
-                                linkHref="#check_eligibility"
-                                text="Check Eligibility"
+                                    <CustomBtn
+                                        linkHref="#check_eligibility"
+                                        text="Check Eligibility"
 
-                            />
-                        </p>
-                        <div className="min-w-[20rem] rounded-lg flex-1 overflow-hidden">
-                            <Image
-                                src={"https://americare.sirv.com/pexels-mart-production-7162989.jpg"}
-                                alt={"Happy teens"}
-                                height={3735}
-                                width={5602}
-                                priority
-                                className={clsx(
-                                    "w-full object-cover smooth",
-                                )}
-                            />
-                        </div>
+                                    />
+                                </p>
+                            </>
+                        </InViewWrapper>
+                        <InViewWrapper animation={cardInAlt} className="min-w-[20rem] flex-1">
+                            <>
+                                <div className="rounded-lg overflow-hidden">
+                                    <Image
+                                        src={"https://americare.sirv.com/pexels-mart-production-7162989.jpg"}
+                                        alt={"Happy teens"}
+                                        height={3735}
+                                        width={5602}
+                                        priority
+                                        className={clsx(
+                                            "w-full object-cover smooth",
+                                        )}
+                                    />
+                                </div>
+                            </>
+                        </InViewWrapper>
                     </div>
                 </div>
-            </div>
+            </InViewWrapper>
         </>
     );
 }
--- app/(pages)/services/components/GappKeyBenefits.tsx
+++ app/(pages)/services/components/GappKeyBenefits.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import nurseSvg from "./components/health-medical-plester-svgrepo-com.svg";
 import careSvg from "./components/health-health-insurance-medical-svgrepo-com.svg";
 import familySvg from "./components/family-svgrepo-com.svg";
+import { Variants } from "framer-motion";
+import InViewWrapper from "@/app/components/InViewWrapper";
 
 const benefitsList: { title: string, content: string, count: number, svg: string }[] = [
     { svg: nurseSvg, title: "Skilled Nursing", content: "In-home skilled nursing care", count: 1 },
@@ -11,24 +13,34 @@ const benefitsList: { title: string, content: string, count: number, svg: string
 ]
 
 export default function GappKeyBenefits() {
+    const cardIn: Variants = {
+        hidden: { opacity: 0, translateX: 100, },
+        visible: { opacity: 1, translateX: 0, transition: { duration: 0.5 } },
+    };
+    const cardInAlt: Variants = {
+        hidden: { opacity: 0, translateX: -100, },
+        visible: { opacity: 1, translateX: 0, transition: { duration: 0.5 } },
+    };
     return (
         <section className={clsx("sm:pt-10 pt-6 w-full mb-12")}>
             <div className="flex flex-wrap gap-4 justify-center w-full perspective">
                 {benefitsList.map(({ content, count, svg, title }) => (
-                    <div key={count} className="md:min-w-[30%] min-w-[20rem] rounded-xl gap-3 flex-1 flex flex-col items-center p-6 text-center dark:bg-white/5 bg-primary/5 smooth rotate-y dark:hover:bg-white/10 hover:bg-primary/10 border hover:border-primary/50 shadow-lg shadow-transparent hover:bg-gradient-to-r from-white/10 to-transparent hover:shadow-primary/5 border-transparent">
-                        <Image
-                            src={svg}
-                            alt="Icon"
-                            height={100}
-                            width={100}
-                            className="sm:w-24 w-20"
-                        />
+                    <InViewWrapper animation={count % 2 === 0 ? cardIn : cardInAlt} key={count} className="md:min-w-[30%] min-w-[20rem] rounded-xl gap-3 flex-1 flex flex-col items-center p-6 text-center dark:bg-white/5 bg-primary/5 smooth rotate-y dark:hover:bg-white/10 hover:bg-primary/10 border hover:border-primary/50 shadow-lg shadow-transparent hover:bg-gradient-to-r from-white/10 to-transparent hover:shadow-primary/5 border-transparent">
+                        <>
+                            <Image
+                                src={svg}
+                                alt="Icon"
+                                height={100}
+                                width={100}
+                                className="sm:w-24 w-20"
+                            />
 
-                        <span className="md:text-2xl sm:text-xl text-primary text-lg font-semibold">{title}</span>
-                        <span>
-                            {content}
-                        </span>
-                    </div>
+                            <span className="md:text-2xl sm:text-xl text-primary text-lg font-semibold">{title}</span>
+                            <span>
+                                {content}
+                            </span>
+                        </>
+                    </InViewWrapper>
                 ))}
             </div>
         </section>
