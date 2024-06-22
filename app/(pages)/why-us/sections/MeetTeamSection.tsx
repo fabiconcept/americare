e01d831e059f07ab2b8f773d5e6062ae90c83393
Fabian Ajokubi <favourajokubi@gmail.com>
2024-06-22 17:24:22 +0100

    Add inView Animation to MeetTeamSection (Why Page)

--- app/(pages)/why-us/sections/MeetTeamSection.tsx
+++ app/(pages)/why-us/sections/MeetTeamSection.tsx
@@ -4,13 +4,20 @@ import TeamMember from "./components/TeamMember";
 import { MeetTeamList } from "@/lib/MeetTeamList";
 import Image from "next/image";
 import arrImg from "@/lib/arrow.svg";
+import { Variants } from "framer-motion";
+import InViewWrapper from "@/app/components/InViewWrapper";
 const FontFamily = Playfair_Display({ subsets: ["latin"], weight: "600" });
 
 export default function MeetTeamSection() {
+    const cardIn: Variants = {
+        hidden: { opacity: 0, translateX: 100, },
+        visible: { opacity: 1, translateX: 0, transition: { duration: 0.5 }},
+    };
+    
     return (
-        <div className="dark:bg-darkBg bg-white grid-bg dark:text-white text-black sm:py-24 py-12 2xl:px-[10vw] sm:px-[8vw]  px-6">
+        <div className="dark:bg-darkBg bg-white grid-bg dark:text-white text-black sm:py-24 py-12 2xl:px-[10vw] sm:px-[8vw] px-6 overflow-x-hidden">
             <div>
-                <div className="mb-12">
+                <InViewWrapper animation={cardIn} className="mb-12">
                     <h1 className={clsx(FontFamily.className, "sm:text-[4vw] text-5xl text-primary font-semibold relative w-fit")}>
                         Meet our team
                         <Image
@@ -21,16 +28,17 @@ export default function MeetTeamSection() {
                             className="h-full object-cover max-md:scale-105 max-md:group-hover:scale-100 smooth absolute -top-2 sm:block hidden -right-[25%]"
                         />
                     </h1>
-                </div>
+                </InViewWrapper>
 
 
                 <div className="grid grid-cols-[repeat(auto-fill,minmax(15rem,1fr))] gap-6">
-                    {MeetTeamList.map(({ id, name, photo, role }) => (
+                    {MeetTeamList.map(({ id, name, photo, role }, index) => (
                         <TeamMember
                             name={name}
                             key={id}
                             photo={photo}
                             role={role}
+                            isOdd={(index + 1) % 2 === 0}
                         />
                     ))}
                 </div>
--- app/(pages)/why-us/sections/components/TeamMember.tsx
+++ app/(pages)/why-us/sections/components/TeamMember.tsx
@@ -1,25 +1,38 @@
+import { Variants } from "framer-motion";
+import InViewWrapper from "@/app/components/InViewWrapper";
 import Image from "next/image";
 
 type TeamMemberProps = {
     name: string;
     role: string;
     photo: string;
+    isOdd: boolean;
 }
 
 export default function TeamMember(prop: TeamMemberProps) {
+    const cardIn: Variants = {
+        hidden: { opacity: 0, translateX: 100, },
+        visible: { opacity: 1, translateX: 0, transition: { duration: 0.5 } },
+    };
+    const cardInAlt: Variants = {
+        hidden: { opacity: 0, translateX: -100, },
+        visible: { opacity: 1, translateX: 0, transition: { duration: 0.5 } },
+    };
     return (
-        <div className="h-[20rem] relative grid place-items-center overflow-hidden rounded-lg group">
-            <Image
-                src={prop.photo}
-                alt={prop.name}
-                height={1000}
-                width={1000}
-                className="h-full object-cover max-md:scale-105 max-md:group-hover:scale-100 smooth"
-            />
-            <div className="absolute sm:-bottom-6 bottom-3 group-hover:bottom-3 delay-75 smooth group-hover:rotate-0 origin-left bg-white/50 backdrop-blur-lg w-[calc(100%-1.5rem)] p-3 px-5 flex flex-col rounded-md">
-                <span className="text-darkBg font-bold max-md:text-base text-lg">{prop.name}</span>
-                <span className="text-black opacity-80 font-semibold max-md:text-sm smooth text-xs group-hover:text-base">{prop.role}</span>
-            </div>
-        </div>
+        <InViewWrapper animation={prop.isOdd ? cardIn : cardInAlt} className="h-[20rem] relative grid place-items-center overflow-hidden rounded-lg group">
+            <>
+                <Image
+                    src={prop.photo}
+                    alt={prop.name}
+                    height={1000}
+                    width={1000}
+                    className="h-full object-cover max-md:scale-105 max-md:group-hover:scale-100 smooth"
+                />
+                <div className="absolute sm:-bottom-6 bottom-3 group-hover:bottom-3 delay-75 smooth group-hover:rotate-0 origin-left bg-white/50 backdrop-blur-lg w-[calc(100%-1.5rem)] p-3 px-5 flex flex-col rounded-md">
+                    <span className="text-darkBg font-bold max-md:text-base text-lg">{prop.name}</span>
+                    <span className="text-black opacity-80 font-semibold max-md:text-sm smooth text-xs group-hover:text-base">{prop.role}</span>
+                </div>
+            </>
+        </InViewWrapper>
     )
 }
