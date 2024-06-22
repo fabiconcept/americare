28be1fc51d7e572d217f6051be5afcd8e4d2ab00
Fabian Ajokubi <favourajokubi@gmail.com>
2024-06-22 06:07:27 +0100

    Add inView Animation to OurServicesSection

--- app/HomePage Sections/OurServicesSection.tsx
+++ app/HomePage Sections/OurServicesSection.tsx
@@ -1,42 +1,54 @@
 import { OurServicesList } from "@/lib/OurServicesList";
 import ServiceItem from "./components/ServiceItem";
 import CustomBtn from "../components/CustomBtn";
+import { Variants } from "framer-motion";
+import InViewWrapper from "../components/InViewWrapper";
 
 export default function OurServicesSection() {
+    const fadeIn: Variants = {
+        hidden: { opacity: 0, translateY: 50 },
+        visible: { opacity: 1, translateY: 0, transition: { duration: 0.5 } },
+    };
+
     return (
         <section className="bg-white py-12 shadow-lg dark:bg-darkBg dark:text-white grid-bg">
-            <h1 className="sm:px-[10vw] mb-3 px-3 text-center text-primary sm:text-[4vw] text-5xl">
-                Our Services
-            </h1>
-            <div className="grid 2xl:grid-cols-[repeat(auto-fill,minmax(30rem,1fr))] sm:grid-cols-[repeat(auto-fill,minmax(25rem,1fr))] sm:px-[3vw] px-3">
-                {OurServicesList.map((serviceItem, index)=>{
-                    if((OurServicesList.length-1) === index) {
-                        return (<>
-                        <div key={"aaec1"} className="sm:block hidden"></div>
-                            <ServiceItem
+            <InViewWrapper animation={fadeIn}>
+                <>
+
+                    <h1 className="sm:px-[10vw] mb-3 px-3 text-center text-primary sm:text-[4vw] text-5xl">
+                        Our Services
+                    </h1>
+                    <div className="grid 2xl:grid-cols-[repeat(auto-fill,minmax(30rem,1fr))] sm:grid-cols-[repeat(auto-fill,minmax(25rem,1fr))] sm:px-[3vw] px-3">
+                        {OurServicesList.map((serviceItem, index) => {
+                            if ((OurServicesList.length - 1) === index) {
+                                return (<>
+                                    <div key={"aaec1"} className="sm:block hidden"></div>
+                                    <ServiceItem
+                                        key={index}
+                                        title={serviceItem.title}
+                                        content={serviceItem.content}
+                                        isOdd={false}
+                                    />
+                                    <div key={"ecaee19"} className="sm:block hidden"></div>
+                                </>)
+                            }
+                            return (<ServiceItem
                                 key={index}
                                 title={serviceItem.title}
                                 content={serviceItem.content}
-                                isOdd={false}
-                            />
-                        <div key={"ecaee19"} className="sm:block hidden"></div>
-                        </>)
-                    }
-                    return (<ServiceItem 
-                        key={index}
-                        title={serviceItem.title}
-                        content={serviceItem.content}
-                        isOdd={index % 2 === 0}
-                    />)
-                })}
-            </div>
-            <div className="grid place-items-center p-8">
-                <CustomBtn
-                    customClass=""
-                    linkHref="#"
-                    text="Learn more"
-                />
-            </div>
+                                isOdd={index % 2 === 0}
+                            />)
+                        })}
+                    </div>
+                    <div className="grid place-items-center p-8">
+                        <CustomBtn
+                            customClass=""
+                            linkHref="#"
+                            text="Learn more"
+                        />
+                    </div>
+                </>
+            </InViewWrapper>
         </section>
     )
 }
--- app/HomePage Sections/components/ServiceItem.tsx
+++ app/HomePage Sections/components/ServiceItem.tsx
@@ -1,13 +1,21 @@
+import InViewWrapper from "@/app/components/InViewWrapper";
 import clsx from "clsx";
+import { Variants } from "framer-motion";
 
-export default function ServiceItem(prop:{ title: string, content: string, isOdd?: boolean }) {
+export default function ServiceItem(prop: { title: string, content: string, isOdd?: boolean }) {
+    const cardIn: Variants = {
+        hidden: { opacity: 0, translateX: 100, },
+        visible: { opacity: 1, translateX: 0, transition: { duration: 0.5 } },
+    };
     return (
-        <div className={clsx(
-            "p-5 flex flex-col gap-3 border-2 border-dotted sm:hover:scale-105 hover:shadow-lg relative hover:z-30 border-transparent smooth hover:border-primary",
-            prop.isOdd ? "dark:bg-[#CDE2BE]/50 hover:dark:bg-[#CDE2BE]/10 bg-[#CDE2BE] hover-bg-odd": "dark:bg-[#F0F7EC]/10 hover:dark:bg-[#F0F7EC]/10 bg-[#F0F7EC] hover-bg"
-        )}>
-            <span className="text-lg font-semibold">{prop.title}</span>
-            <p>{prop.content}</p>
-        </div>
+        <InViewWrapper animation={cardIn}>
+            <div className={clsx(
+                "p-5 flex flex-col gap-3 border-2 border-dotted sm:hover:scale-105 hover:shadow-lg relative hover:z-30 border-transparent smooth hover:border-primary",
+                prop.isOdd ? "dark:bg-[#CDE2BE]/50 hover:dark:bg-[#CDE2BE]/10 bg-[#CDE2BE] hover-bg-odd" : "dark:bg-[#F0F7EC]/10 hover:dark:bg-[#F0F7EC]/10 bg-[#F0F7EC] hover-bg"
+            )}>
+                <span className="text-lg font-semibold">{prop.title}</span>
+                <p>{prop.content}</p>
+            </div>
+        </InViewWrapper>
     )
 }
